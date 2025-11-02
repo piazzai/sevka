683525482966bb5fed4d322da13734a264559396
piazzai <42124135+piazzai@users.noreply.github.com>
2025-11-02 10:26:11 +0000

    Reorganize dist folders

--- subset.sh
+++ subset.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-for CHARSET in LATIN US_ASCII; do
+for CHARSET in us_ascii latin; do
   for EXT in ttf woff2; do
     for FACE in Sevka SevkaSlab SevkaFixed; do
-      mkdir -p dist/$CHARSET/$EXT/$FACE
+      mkdir -p dist/${CHARSET/us\_/}/$EXT/$FACE
       FILES=$(ls dist/$FACE/${EXT^^})
       for FILE in $FILES; do
-        glyphhanger --subset="dist/$FACE/${EXT^^}/$FILE" --formats=$EXT --$CHARSET
-        mv "dist/$FACE/${EXT^^}/${FILE%%."$EXT"}-subset.$EXT" "dist/$CHARSET/$EXT/$FACE/$FILE"
+        glyphhanger --subset="dist/$FACE/${EXT^^}/$FILE" --formats=$EXT --${CHARSET^^}
+        mv "dist/$FACE/${EXT^^}/${FILE%%."$EXT"}-subset.$EXT" "dist/${CHARSET/us_/}/$EXT/$FACE/$FILE"
       done
     done
   done
